ad171c8d0f3fe04893e4b912c3d0b639a3fe6dd9
RyushiAok <55625375+RyushiAok@users.noreply.github.com>
2024-05-11 02:12:14 +0900

    投稿・閲覧ページ雛形

--- client/src/app/layout.tsx
+++ client/src/app/layout.tsx
@@ -2,6 +2,9 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 // import "./globals.css";
 
+import { Theme } from "@radix-ui/themes";
+import "@radix-ui/themes/styles.css";
+
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
@@ -16,7 +19,15 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <Theme
+          style={{
+            lineHeight: 1.6,
+          }}
+        >
+          {children}
+        </Theme>
+      </body>
     </html>
   );
 }
--- client/src/components/post/index.tsx
+++ client/src/components/post/index.tsx
@@ -1,37 +1,72 @@
-type Props = {
-  userName: string;
-  contents: string;
-  reactions: {
-    kind: "like" | "love";
-    x: number;
-    y: number;
-    theta: number;
-    scale: number;
-  }[];
+import { gray } from "@radix-ui/colors";
+export type Reaction = {
+  kind: "like" | "love";
+  x: number;
+  y: number;
+  theta: number;
+  scale: number;
 };
 
-export const Post: React.FC<Props> = ({ userName, contents, reactions }) => {
+export type Post = {
+  contents: string;
+  reactions: Reaction[];
+};
+
+export type Props = {
+  post: Post;
+  onClick?: (x: number, y: number) => void;
+};
+
+export const Post: React.FC<Props> = ({ post, onClick }) => {
   return (
-    <div>
-      <div>
-        <h2>{userName}</h2>
-        <p>{contents}</p>
+    <div
+      style={{
+        border: `2px solid ${gray.gray3}`,
+        borderRadius: "1rem",
+        position: "relative",
+        overflow: "hidden",
+        padding: "0.5rem 1.2rem",
+        width: "100%",
+      }}
+      onClick={(e) => {
+        const rect = e.currentTarget.getBoundingClientRect();
+        const relX = e.clientX - rect.left;
+        const relY = e.clientY - rect.top;
+        onClick?.(relX / rect.width, relY / rect.height);
+      }}
+    >
+      <div
+        style={{
+          display: "flex",
+          flexDirection: "column",
+          zIndex: 10,
+          position: "relative",
+        }}
+      >
+        <p
+          style={{
+            fontSize: "1.25rem",
+            fontWeight: "bold",
+          }}
+        >
+          {post.contents}
+        </p>
       </div>
-      <div style={{ position: "relative" }}>
-        {reactions.map((reaction, index) => (
-          <div
-            key={index}
-            style={{
-              position: "absolute",
-              top: `${reaction.y}%`,
-              left: `${reaction.x}%`,
-              transform: `rotate(${reaction.theta}rad) scale(${reaction.scale})`,
-            }}
-          >
-            {reaction.kind === "like" ? <> 👍 </> : <> 💛</>}
-          </div>
-        ))}
-      </div>
+      {post.reactions.map((reaction, index) => (
+        <div
+          key={index}
+          style={{
+            zIndex: 1,
+            position: "absolute",
+            top: `${reaction.y}%`,
+            left: `${reaction.x}%`,
+            userSelect: "none",
+            transform: `translate(-50%, -50%) rotate(${reaction.theta}rad) scale(${reaction.scale})`,
+          }}
+        >
+          {reaction.kind === "like" ? <> 👍 </> : <> 💛</>}
+        </div>
+      ))}
     </div>
   );
 };
